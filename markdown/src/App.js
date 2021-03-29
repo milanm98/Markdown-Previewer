ff91dd4d2bb2d5b748943d6cf35b4805a4c64506
milanm98 <marjanovmilan13@gmail.com>
2021-03-29 14:55:30 -0400

    design updated

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+    <div className="h-screen w-screen background">
     </div>
+    <h1 className="text-black font-black text-center text-5xl title">Markdown Previewer</h1>
+    <div className="flex flex-col md:flex-row gap-9 center w-5/6 h-5/6">
+      <div className="text-4xl w-3/6 h-3/6 ring 8 text-black font-black text-center bg-white">
+        <h1 className="border">Editor</h1>
+        <textarea className="w-full h-full"></textarea>
+      </div>
+      <div className="text-4xl bg-white w-3/6 h-3/6 ring 8 text-black font-black text-center">
+        <h1 className="">Preview</h1>
+      <div className="">
+      </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
