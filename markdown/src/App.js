0bb3c8f8223e4f4ff038446e006d2cc5f4d8cee8
milanm98 <marjanovmilan13@gmail.com>
2021-03-29 15:26:27 -0400

    design done

--- markdown/src/App.js
+++ markdown/src/App.js
@@ -5,15 +5,18 @@ function App() {
     <main>
     <div className="h-screen w-screen background">
     </div>
-    <h1 className="text-black font-black text-center text-5xl title">Markdown Previewer</h1>
+    <h1 className="text-black font-black text-center text-2xl md:text-5xl title">Markdown Previewer</h1>
     <div className="flex flex-col md:flex-row gap-9 center w-5/6 h-5/6">
-      <div className="text-4xl w-3/6 h-3/6 ring 8 text-black font-black text-center bg-white">
+      <div className="text-1xl md:text-4xl w-7/8 md:w-3/6 h-2/6 md:h-3/6 text-black font-black text-center bg-white">
         <h1 className="border">Editor</h1>
-        <textarea className="w-full h-full"></textarea>
+        <div className="w-full h-full">
+          <textarea className="w-full h-full"></textarea>
+        </div>
       </div>
-      <div className="text-4xl bg-white w-3/6 h-3/6 ring 8 text-black font-black text-center">
-        <h1 className="">Preview</h1>
-      <div className="">
+      <div className="text-1xl md:text-4xl bg-white w-7/8 md:w-3/6 h-2/6 md:h-3/6 text-black font-black text-center">
+        <h1 className="border">Preview</h1>
+      <div className="w-full h-full bg-white">
+        
       </div>
         </div>
       </div>
